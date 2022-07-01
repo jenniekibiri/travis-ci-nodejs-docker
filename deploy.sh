0ab14d07271383eb62102e3384af1c9015624748
jenny <josphatje@gmail.com>
2022-07-01 08:48:23 +0300

    encode ssh

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,11 @@ docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
 
 # Decode SSH key
-echo "${SSH_KEY}" | base64 -di > ssh_key
+echo ${SSH_KEY} | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
-# Add production server to known hosts
-echo "${SSH_HOST}" | base64 -di >> ~/.ssh/known_hosts
+# # Add production server to known hosts
+# echo "${SSH_HOST}" | base64 -di >> ~/.ssh/known_hosts
 
 
 
