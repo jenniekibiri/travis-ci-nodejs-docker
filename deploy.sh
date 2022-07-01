d57f21c61f25c41986cd0fcf67bfc441864bb8ad
jenny <josphatje@gmail.com>
2022-07-01 08:24:45 +0300

    add base 64

--- deploy.sh
+++ deploy.sh
@@ -16,15 +16,17 @@ docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
 
 # Decode SSH key
-echo "${SSH_KEY}" | base64 -d > ssh_key
+echo "${SSH_KEY}" | base64 -di > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
 # Add production server to known hosts
-echo "${SSH_HOST}" | base64 -d >> ~/.ssh/known_hosts
+echo "${SSH_HOST}" | base64 -di >> ~/.ssh/known_hosts
 
 
 echo "Deploying via remote SSH"
-ssh  "root@${SSH_HOST} -o StrictHostKeyChecking=no" \
+# ssh into the server and run the following commands
+
+ssh  "root@${SSH_HOST}" \
   "echo "${PASSWORD}" | docker login -u "${USERNAME}" --password-stdin \
   && docker pull ${IMAGE_NAME}:${IMAGE_TAG} \
   && docker stop autodeploy-docker \
