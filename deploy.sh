9996c3c32e59c424acccc95c5432d526e75628cc
jenny <josphatje@gmail.com>
2022-07-01 08:40:00 +0300

    encode ssh

--- deploy.sh
+++ deploy.sh
@@ -15,19 +15,25 @@ echo "${PASSWORD}" | docker login -u "${USERNAME}" --password-stdin
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
 
+# Decode SSH key
+echo "${SSH_KEY}" | base64 -d > ssh_key
+chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
+
+# Add production server to known hosts
+echo "${SSH_HOST}" | base64 -d >> ~/.ssh/known_hosts
 
 
 
-echo "Deploying via remote SSH"
-# ssh into the server and run the following commands
-ssh \
-    -o UserKnownHostsFile= ~/.ssh/known_hosts \
-    -o StrictHostKeyChecking=yes  "root@${SSH_HOST}" \
-  "echo "${PASSWORD}" | docker login -u "${USERNAME}" --password-stdin \
-  && docker pull ${IMAGE_NAME}:${IMAGE_TAG} \
-  && docker stop autodeploy-docker \
-  && docker rm autodeploy-docker \
-  && docker run --init -d --name autodeploy-docker -p 3000:3000 ${IMAGE_NAME}:${IMAGE_TAG} \
-  && docker system prune -af" # remove unused images to free up space
+# echo "Deploying via remote SSH"
+# # ssh into the server and run the following commands
+# ssh \
+#     -o UserKnownHostsFile= ~/.ssh/known_hosts \
+#     -o StrictHostKeyChecking=yes  "root@${SSH_HOST}" \
+#   "echo "${PASSWORD}" | docker login -u "${USERNAME}" --password-stdin \
+#   && docker pull ${IMAGE_NAME}:${IMAGE_TAG} \
+#   && docker stop autodeploy-docker \
+#   && docker rm autodeploy-docker \
+#   && docker run --init -d --name autodeploy-docker -p 3000:3000 ${IMAGE_NAME}:${IMAGE_TAG} \
+#   && docker system prune -af" # remove unused images to free up space
 
-echo "Successfully deployed, hooray!"
+# echo "Successfully deployed, hooray!"
